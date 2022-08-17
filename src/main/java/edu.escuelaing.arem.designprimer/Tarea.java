f2cd49c6040b082092419618585a39cbbfc23da1
clases clases <clases@labinfo.is.escuelaing.edu.co>
2022-08-17 18:43:47 -0500

    Comienzo

---
+++ src/main/java/edu.escuelaing.arem.designprimer/Tarea.java
@@ -0,0 +1,21 @@
+package edu.escuelaing.arem.designprimer;
+
+import static spark.Spark.*;
+
+public class Tarea {
+    public static void main(String[] args) {
+        port(getPort());
+        staticFiles.location("/public");
+//        get("/hello", (req, res) -> "Hello Heroku");
+        get("/intraday", (req, res) -> {
+            res.type("application/json");
+            return HttpConnection.getAPI();
+        });
+    }
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+}
